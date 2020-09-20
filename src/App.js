6e70ac47360ed72cc02e0b8242109a74076dc52e
michaelscoggins <rhythmghosts@gmail.com>
2020-09-20 00:05:46 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import './App.css';
+// import FirstComponent from './firstComponent'
 
-function App() {
-  return (
-    <div className="App">
-      First React Project    
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOn: false 
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>First React Project</h1>
+        <button>Submit</button>
+      </div>
+    );
+    }
 }
 
 export default App;
---
+++ src/firstComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FirstComponent() {
+  return (
+    <h1>Hello Freak Bitches</h1>
+  )
+}
+
+export default FirstComponent
