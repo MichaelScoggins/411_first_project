92f0bfe19c53e5b03b00908aeb8f716ce87d8c42
michaelscoggins <rhythmghosts@gmail.com>
2020-09-21 14:03:34 -0500

    fixed component name; capitalized it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component }  from 'react';
 import './App.css';
-import FirstComponent from './firstComponent'
+import FirstComponent from './FirstComponent'
 
 class App extends Component {
   state = {
